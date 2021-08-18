39620d72fa10254ee9e1fb55f6a3d5b6c2f257cb
vldf <vladislavf7@gmail.com>
2021-08-18 22:12:40 +0300

    Add LibSL class

---
+++ src/main/kotlin/org/jetbrains/research/libsl/LibSL.kt
@@ -0,0 +1,48 @@
+package org.jetbrains.research.libsl
+
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.tree.ParseTreeListener
+import org.jetbrains.research.libsl.asg.Library
+import org.jetbrains.research.libsl.asg.LslContext
+import org.jetbrains.research.libsl.visitors.ASGBuilder
+import org.jetbrains.research.libsl.visitors.Resolver
+import java.io.File
+import java.nio.file.Path
+
+class LibSL(
+    private val basePath: String
+) {
+    val context = LslContext()
+    lateinit var library: Library
+    private var isParsed = false
+
+    var errorListener: ParseTreeListener? = null
+    
+    fun loadFromString(string: String): Library {
+        val stream = CharStreams.fromString(string)
+        val lexer = LibSLLexer(stream)
+        val tokenStream = CommonTokenStream(lexer)
+        val context = LslContext()
+        context.init()
+        val parser = LibSLParser(tokenStream)
+        if (errorListener != null) {
+            parser.addParseListener(errorListener)
+        }
+
+        val file = parser.file()
+        Resolver(context,basePath).visitFile(file)
+        return ASGBuilder(context).visitFile(file)
+    }
+
+    fun loadFromFile(file: File): Library {
+        return loadFromString(file.readText())
+    }
+
+    fun loadByPath(path: Path): Library {
+        return loadFromFile(path.toFile())
+    }
+
+    fun loadFromPath(path: String): Library {
+        return loadFromFile(File(path))
+    }
+}
